4330a97da2b78314b42692f4c40b39b17efd7fef
Akaribu <50196691+Akaribu@users.noreply.github.com>
2019-05-14 00:32:18 +0900

    Update response.js

--- response.js
+++ response.js
@@ -1,28 +1,36 @@
 function reload(r) {
-	if(r.sender == '니부아카리' || r.sender == '건의방'){
-		reloadcheck = 1;
-		reloadtime = new Date().getTime();
-		var Timer = new Date();
-	    file = "storage/emulated/0/kbot/response.js";
-	    checksum = org.jsoup.Jsoup.connect("https://github.com/Akaribu/KakaoTalkBot/commits/master").get().select("div.repository-content>a").attr("href").split('commit/')[1];
-	    conn = new java.net.URL("https://raw.githubusercontent.com/Akaribu/KakaoTalkBot/"+checksum+"/response.js").openConnection();
-	    br = new java.io.BufferedReader(new java.io.InputStreamReader(conn.getInputStream()));
-	    str = "";
-	    tmp = null;
-	    while ((tmp = br.readLine()) != null) {
-	        str += tmp + "\n";
-	    }
-	    var filedir = new java.io.File(file);
-	    var bw = new java.io.BufferedWriter(new java.io.FileWriter(filedir));
-	    bw.write(str.toString());
-	    bw.close();
-	    var time = (new Date() - Timer) / 1000;
-	    Api.replyRoom(r.room ,"파일저장 완료 / " + time + "s\n" + new Date() );
-	    Api.reload();
-	    reloadcheck = 0;
-	    var time = (new Date() - Timer) / 1000;
-	    Api.replyRoom(r.room , "reloading 완료 / " + time + "s\n" + new Date());
-	}
+    try {
+        if (r.sender == "니부아카리" || r.sender == "46") {
+            reloadcheck = 1;
+            reloadtime = new Date().getTime();
+            var Timer = new Date();
+            file = "storage/emulated/0/kbot/response.js";
+           checksum = org.jsoup.Jsoup.connect("https://github.com/Akaribu/KakaoTalkBot/commits/master").get().select("div.repository-content>a").attr("href").split('commit/')[1];
+conn = new java.net.URL("https://raw.githubusercontent.com/Akaribu/KakaoTalkBot/" + checksum + "/response.js").openConnection();
+            br = new java.io.BufferedReader(new java.io.InputStreamReader(conn.getInputStream()));
+            str = "";
+            tmp = null;
+            while ((tmp = br.readLine()) != null) {
+                str += tmp + "\n";
+            }
+            var filedir = new java.io.File(file);
+            var bw = new java.io.BufferedWriter(new java.io.FileWriter(filedir));
+            bw.write(str.toString());
+            bw.close();
+            var time = (new Date() - Timer) / 1000;
+            r.replier.reply("파일저장 완료 / " + time + "s\n" + new Date());
+            T.interrupt();
+            Api.reload();
+            reloadcheck = 0;
+            control = D.selectForArray("control").map(v => v[0]);
+            controlPanel = D.selectForObject("control");
+            var time = (new Date() - Timer) / 1000;
+            r.replier.reply("reloading 완료 / " + time + "s\n" + new Date());
+        }
+    }
+    catch (e) {
+        r.replier.reply("46", e + "\n" + e.stack);
+    }
 }
 function response(room, msg, sender, isGroupChat, replier, imageDB) {
 	var r = { replier: replier, msg: msg, sender: sender, room: room, imageDB :imageDB};
