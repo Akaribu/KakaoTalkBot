9d348c3d4d1159fd07c1a9daa21179d083874f20
Akaribu <50196691+Akaribu@users.noreply.github.com>
2019-05-14 21:46:02 +0900

    Update response.js

--- response.js
+++ response.js
@@ -243,7 +243,7 @@ function response(room, msg, sender, isGroupChat, replier, imageDB) {
 	botpoint(r);
 	pointcheck(r);
 	pointgive(r);
-	if (msg.indexOf("/가사")==0&&room=="46"){
+	if (msg.indexOf("/가사"){
 	lyric(r);
 	return;
 	}
