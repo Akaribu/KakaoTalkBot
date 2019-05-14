0cf5524d84c8251817e98f278ae7de8d05a3035e
Akaribu <50196691+Akaribu@users.noreply.github.com>
2019-05-15 00:57:12 +0900

    Update response.js

--- response.js
+++ response.js
@@ -42,7 +42,7 @@ function pointgive(r){
 		random = Math.floor(Math.random()*101);
 		give = Math.floor(Math.random()*31);
 		currentpoint=D.selectForArray("botpoint",null,"room=? and name=?",[r.room,r.sender])[0][2];
-			if(random > 95 && r.room=="46"){
+			if(random > 98 && r.room=="46"){
 				currentpoint+=give;
 				D.update("botpoint",{"point":currentpoint},"name=?",r.sender);
 				r.replier.reply(r.sender+"님 "+give+"네루 획득")
