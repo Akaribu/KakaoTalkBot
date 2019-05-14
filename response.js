e9fb1de1198dc1b7aed2a127eee3b905d6245359
Akaribu <50196691+Akaribu@users.noreply.github.com>
2019-05-14 21:53:03 +0900

    Update response.js

--- response.js
+++ response.js
@@ -14,7 +14,7 @@ var D = require("DBManager.js")("D");
     var room = r.r;
     var sender = r.s;
     var msg = r.m;
-    var str = msg.substr(4).rmspace();
+    var str = msg.str.split("사")[0] .rmspace();
     var title = str.includes("/") ? str.split("/")[0] : str;
     var artist = str.includes("/") ? str.split("/")[1] : "";
     var xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://www.w3.org/2003/05/soap-envelope\"" + " xmlns:SOAP-ENC=\"http://www.w3.org/2003/05/soap-encoding\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" " + "xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:ns2=\"ALSongWebServer/Service1Soap\" xmlns:ns1=\"ALSongWebServer\" " + "xmlns:ns3=\"ALSongWebServer/Service1Soap12\"><SOAP-ENV:Body><ns1:GetResembleLyric2><ns1:stQuery><ns1:strTitle>" + title.XMLEncode() + "</ns1:strTitle><ns1:strArtistName>" + artist.XMLEncode() + "</ns1:strArtistName><ns1:nCurPage>0</ns1:nCurPage></ns1:stQuery>" + "</ns1:GetResembleLyric2></SOAP-ENV:Body></SOAP-ENV:Envelope>";
