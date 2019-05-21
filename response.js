dd6d913b480095426539227577a9eaf29003e136
Akaribu <50196691+Akaribu@users.noreply.github.com>
2019-05-21 20:47:01 +0900

    Update response.js

--- response.js
+++ response.js
@@ -539,25 +539,23 @@ function Hinataosirase(){
  var link = "www.hinatazaka46.com"+org.jsoup.Jsoup.connect("https://www.hinatazaka46.com/s/official/news/list?ima=0000&dy=201905").get().select("p.c-news__text").get(0).parent().attr("href")
  var doc = temp+"\n"+link
  var difcount = 0;
-if(difcount = 0){
- for(var i=0; i<1;i++){
+ if(difcount = 0){
    if(D.selectForArray('Hinata')[0][0].indexOf(temp1.get(0).text()) == 0){
-    break;
+  
    }
    else{
     difcount += 1;
-       break;
+       
               }
   
+ 
   }
- }
  if(difcount > 0){
   D.update('Hinata', { name : temp1.get(0).text()});
-       
+     
   Api.replyRoom("건의방","새공지!\n"+doc);
   Api.replyRoom("46","새공지!\n"+doc);
-difcount-=1
-break;
+  difcount-=1
  } 
 }
  catch(e){
@@ -572,30 +570,26 @@ function Keyakiosirase(){
  var doc = temp+"\n"+link
  var difcount = 0;
  if(difcount = 0){
-for(var i=0; i<1;i++){
    if(D.selectForArray('Keyaki')[0][0].indexOf(temp1.get(0).text()) == 0){
-    break;
    }
    else{
     difcount += 1;
-       break;
+      
               }
-  }
+  
  }
- if(difcount > 0){
+ if(difcount = 0){
   D.update('keyaki', { name : temp1.get(0).text()}); 
-               
+ 
   Api.replyRoom("건의방","새공지!\n"+doc);
   Api.replyRoom("46","새공지!\n"+doc);
-difcount-=1
-break;
+  difcount-=1;
  } 
 }
  catch(e){
  Api.replyRoom('건의방',e+"\n"+e.stack);
  }
 }
-
 var nofinication1 = T.register("Hinata",()=>{
 	while(1){
 		java.lang.Thread.sleep(50*1000);
