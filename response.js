71ab85fc70815372c2d91ab668ac215751664793
Akaribu <50196691+Akaribu@users.noreply.github.com>
2019-05-22 01:57:33 +0900

    Update response.js

--- response.js
+++ response.js
@@ -538,7 +538,7 @@ function Hinataosirase(){
  var temp1=org.jsoup.Jsoup.connect("https://www.hinatazaka46.com/s/official/news/list?ima=0000&dy=201905").get().select("p.c-news__text");
  var link = "www.hinatazaka46.com"+org.jsoup.Jsoup.connect("https://www.hinatazaka46.com/s/official/news/list?ima=0000&dy=201905").get().select("p.c-news__text").get(0).parent().attr("href")
  var doc = temp+"\n"+link
- var count == 0;
+ var count = 0;
  if(count == 0){
    if(D.selectForArray('Hinata')[0][0].indexOf(temp1.get(0).text()) == 0){}
    else{
@@ -567,7 +567,7 @@ function Keyakiosirase(){
  var temp1=org.jsoup.Jsoup.connect("http://www.keyakizaka46.com/s/k46o/news/list?ima=0000&dy=201905").get().select("div.text")
  var link = org.jsoup.Jsoup.connect("http://www.keyakizaka46.com/s/k46o/news/list?ima=0000&dy=201905").get().select("div.text").get(0).select("a").attr("abs:href")
  var doc = temp+"\n"+link
- var count == 0;
+ var count = 0;
  if(count == 0){
    if(D.selectForArray('Keyaki')[0][0].indexOf(temp1.get(0).text()) == 0){
 
