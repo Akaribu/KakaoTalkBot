221a31debf566ba79969001fa1dc16ab451ec5eb
Akaribu <50196691+Akaribu@users.noreply.github.com>
2019-05-25 02:48:59 +0900

    Update response.js

--- response.js
+++ response.js
@@ -540,9 +540,9 @@ function Hinataosirase(){
  var doc = temp+"\n"+link
  var counter= Number(0)
  var verse = D.selectForArray('Count')[0][0] 
- if(verse= Number(1)){
+ if(verse= Number(0)){
    if(D.selectForArray('Hinata')[0][0].indexOf(temp1.get(0).text()) == 0){
-	   counter=1;
+	   counter=0;
 	   D.update("Count", {"point":counter})
 	   Api.replyRoom("건의방",counter)}
   
