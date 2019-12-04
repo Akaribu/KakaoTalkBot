c72d57b2559291dd49ee992fd212dd75bfa844e8
Akaribu <50196691+Akaribu@users.noreply.github.com>
2019-12-04 22:44:55 +0900

    Update response.js

--- response.js
+++ response.js
@@ -625,7 +625,7 @@ function intro(r){
 Hinata =function () {
 	name = org.jsoup.Jsoup.connect("https://www.hinatazaka46.com/s/official/?ima=0000").get().select('p.c-news__text').toArray().map((v)=>v.text())[0]
 	link = "https://www.hinatazaka46.com"+org.jsoup.Jsoup.connect("https://www.hinatazaka46.com/s/official/?ima=0000").get().select('a').attr("href")
-	if(D.selectForArray("hinata") != link){
+	if(D.selectForArray("hinata")[0] != link){
 	D.insert("hinata",{ osirase : link })
 	Api.replyRoom("건의방","히나타자카46 공지가 갱신 되었습니다.\n"+name+"\n"+link)
 	}
