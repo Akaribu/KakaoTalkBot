3a3a356dd0a4156c469f115340f242e9bc362f88
Akaribu <50196691+Akaribu@users.noreply.github.com>
2019-12-04 19:53:23 +0900

    Update response.js

--- response.js
+++ response.js
@@ -628,14 +628,27 @@ function chat(r){
 	if(r.msg.indexOf("/최근채팅")==0){
 		namae = r.msg.split(" ")[1];
 		numb = r.msg.split(" ")[2];
-		number = Number(D.selectForArray("chatdb","chat","room=? and name=?",[r.room,namae]).length);
-		name = Number(D.selectForArray("chatdb","name","room=? and name=?",[r.room,namae]).length);
+		if(namae=null && numb=null)
+		{
+		number = Number(D.selectForArray("chatdb","chat","room=?",[r.room]).length-2);
+		name = Number(D.selectForArray("chatdb","name","room=?",[r.room]).length-2);
+		for(i=0; i<5 ; i++){
+			ch = D.selectForArray("chatdb","chat","room=?",[r.room])[number];
+			na = D.selectForArray("chatdb","name","room=? ",[r.room])[name];	
+	        	arr[i]=na+" : "+ch
+			name=name-1
+			number=number-1
+		}
+		else{
+		number = Number(D.selectForArray("chatdb","chat","room=? and name=?",[r.room,namae]).length-1);
+		name = Number(D.selectForArray("chatdb","name","room=? and name=?",[r.room,namae]).length-1);
 		for(i=0 ; i<numb ; i++){
 		ch = D.selectForArray("chatdb","chat","room=?",[r.room])[number];
 		na = D.selectForArray("chatdb","name","room=? ",[r.room])[name];	
 	        arr[i]=na+" : "+ch
 		name=name-1
 		number=number-1
+		}
 		}
 	r.replier.reply(arr.join("\n"))
 	}
