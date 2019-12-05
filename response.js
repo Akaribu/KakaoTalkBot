8ecb49a69731fad3819a8ee2e17889780b1ebe74
Akaribu <50196691+Akaribu@users.noreply.github.com>
2019-12-05 11:37:50 +0900

    Update response.js

--- response.js
+++ response.js
@@ -726,6 +726,7 @@ function response(room, msg, sender, isGroupChat, replier, imageDB) {
 	I.run(room, sender, msg);
 	pointgive(r);
 	hinatanofi
+        Hinata(r)
 	pointcheck(r);
 	intro(r);
 	versus(r);
