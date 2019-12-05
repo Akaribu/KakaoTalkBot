7c33ea9f5028da425423609e19fc6206bffd6075
Akaribu <50196691+Akaribu@users.noreply.github.com>
2019-12-06 01:39:35 +0900

    Update response.js

--- response.js
+++ response.js
@@ -622,7 +622,7 @@ function intro(r){
 		r.replier.reply("10 네루를 사용해 부방장 복권을 뽑습니다.\n부방장 당첨 확률 1% 당첨 시 하루 당 200 네루 지급 부방장은 2 명으로 2명인 상태에서 새로운 부방장이 뽑히면 첫번째로 부방장이 된 사람은 탄핵됩니다.");
 	   }
 }
-function Hinata(r) {
+Hinata =function () {
 	name = org.jsoup.Jsoup.connect("https://www.hinatazaka46.com/s/official/?ima=0000").get().select('p.c-news__text').toArray().map((v)=>v.text())[0]
 	link = "https://www.hinatazaka46.com"+org.jsoup.Jsoup.connect("https://www.hinatazaka46.com/s/official/?ima=0000").get().select('li.p-news__item').select('a').attr("href")
 	if(JSON.stringify(D.selectForArray('Hinata')[0]) != link){
@@ -630,7 +630,7 @@ function Hinata(r) {
 	Api.replyRoom("46","히나타자카46 공지가 갱신 되었습니다.\n"+name+"\n"+link)
 	}
 }
-hinatanofi = T.register("Hinata",()=>{
+hinatanofi = T.register("Hinata(r)",()=>{
 		while(true){
 			java.lang.Thread.sleep(50*1000);
 			Hinata(r);
