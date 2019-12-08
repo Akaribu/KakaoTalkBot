ac9e377aba5dbe0091738c2707af132bfa5d5048
Akaribu <50196691+Akaribu@users.noreply.github.com>
2019-12-08 19:53:21 +0900

    Update response.js

--- response.js
+++ response.js
@@ -707,7 +707,7 @@ function pointgive(r){
 	give = Number(0)
 	currentpoint=D.selectForArray("botpoint",null,"room=? and name=?",[r.room,r.sender])[0][2];
 	if(D.selectForArray("botpoint","name","name=?",r.sender) == r.sender){
-		if(random > 10 && r.room=="46"){
+		if(random > 95 && r.room=="46"){
 			if(currentpoint>=200){
 				Number(0)<=Number(give)<=Number(9)
 			}
