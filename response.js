601cbb857d98da685460b39e88eca36638748b9b
Akaribu <50196691+Akaribu@users.noreply.github.com>
2019-12-08 18:19:39 +0900

    Update response.js

--- response.js
+++ response.js
@@ -671,7 +671,7 @@ keyakinofi = T.register("Keyaki1",()=>{
 		Keyaki();
 		}
 	}).start();
-function Keyaki() {
+function Keyaki(r) {
 	name=org.jsoup.Jsoup.connect("https://www.keyakizaka46.com/s/k46o/?ima=0000").get().select("span").toArray().map((v)=>v.text())[0]
 	link="https://www.keyakizaka46.com"+org.jsoup.Jsoup.connect("https://www.keyakizaka46.com/s/k46o/?ima=0000").get().select("span").select("a").attr("href")
 	if((D.selectForArray('Keyaki')[0] != link)==true){
@@ -679,7 +679,7 @@ function Keyaki() {
 	Api.replyRoom("46","케야키자46 공지가 갱신 되었습니다.\n"+name+"\n"+link)
 	}
 }	
-function Hinata() {
+function Hinata(r) {
 	name = org.jsoup.Jsoup.connect("https://www.hinatazaka46.com/s/official/?ima=0000").get().select('p.c-news__text').toArray().map((v)=>v.text())[0]
 	link = "https://www.hinatazaka46.com"+org.jsoup.Jsoup.connect("https://www.hinatazaka46.com/s/official/?ima=0000").get().select('li.p-news__item').select('a').attr("href")
 	if((D.selectForArray('Hinata')[0] != link)==true){
