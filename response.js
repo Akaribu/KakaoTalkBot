d309e6c6f508951577f7afa927ebcd26ad504f1e
Akaribu <50196691+Akaribu@users.noreply.github.com>
2019-12-08 19:32:38 +0900

    Update response.js

--- response.js
+++ response.js
@@ -704,7 +704,7 @@ function pointcheck(r){
 }
 function pointgive(r){
 	random = Math.floor(Math.random()*101);
-	give = Number(1);
+	give = Number();
 	currentpoint=D.selectForArray("botpoint",null,"room=? and name=?",[r.room,r.sender])[0][2];
 	if(D.selectForArray("botpoint","name","name=?",r.sender) == r.sender){
 		if(random > 95 && r.room=="46"){
